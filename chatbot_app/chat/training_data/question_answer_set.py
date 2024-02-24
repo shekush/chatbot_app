b6875b8e4fbd0246da1f33eb68da9973c8b31dba
Shejal <shejal270999@gmail.com>
2024-02-24 13:47:22 +0530

    add project folder

---
+++ chatbot_app/chat/training_data/question_answer_set.py
@@ -0,0 +1,18 @@
+list_to_train = [
+    "Hi",
+    "Hello, how can I help you",
+    "What is your name?",
+    "I am just a ChatBot. Care to give me a name?",
+    "How are you?",
+    "I'm great! Thanks for asking.",
+    "Thanks",
+    "You're Welcome",
+    "What can you do?",
+    "I can chat with you :)",
+    "Who are you?",
+    "I am a ChatBot",
+]
+
+
+def get_list_to_train():
+    return list_to_train
